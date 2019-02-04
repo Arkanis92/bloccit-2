b63d2102cd0b58c0f7243e08bb9c947a67496379
Tanya Swift <tanyanicolerussi@gmail.com>
2019-02-04 08:49:48 -0700

    Checkpoint Routing Controllers and the View Draft 1

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -19,15 +19,16 @@ describe("routes : static", () => {
 
   describe("GET /marco", () => {
 
-    it("should return status code 200", (done) => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
 
       request.get(base, (err, res, body) => {
         expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
 
         done();
       });
     });
 
   });
-  
+
 });
