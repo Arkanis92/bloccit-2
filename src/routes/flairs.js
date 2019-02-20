06fa3d89f583eb6daec677a7cf07542ae9455801
Tanya Swift <tanyanicolerussi@gmail.com>
2019-02-20 16:35:18 -0700

    Assignment Post Resource Part 2 Take 2 Draft 2

--- src/routes/flairs.js
+++ src/routes/flairs.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const flairController = require("../controllers/flairController");
 
 router.get("/topics/:topicId/posts/:postId/flairs/new", flairController.new);
