1dd157fd8554bdcd091be9ae4d6816e9d2f0d8f8
Tanya Swift <tanyanicolerussi@gmail.com>
2019-02-22 19:59:02 -0700

    Checkpoint Validations Take 2 Draft 3

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const postController = require("../controllers/postController")
+const postController = require("../controllers/postController");
 const validation = require("./validation");
 
 router.get("/topics/:topicId/posts/new", postController.new);
