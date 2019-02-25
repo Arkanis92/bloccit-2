80b4cbeba8a7fa6447efbb7cf19e3c3a60f7b786
Tanya Swift <tanyanicolerussi@gmail.com>
2019-02-25 07:55:21 -0700

    Assignment Authorization Draft 2

--- src/db/queries.posts.js
+++ src/db/queries.posts.js
@@ -21,32 +21,47 @@ module.exports = {
     })
   },
   deletePost(id, callback){
-   return Post.destroy({
-     where: { id }
-   })
-   .then((deletedRecordsCount) => {
-     callback(null, deletedRecordsCount);
-   })
-   .catch((err) => {
-     callback(err);
-   })
- },
-  updatePost(id, updatedPost, callback){
+    return Post.findById(req.params.id)
+    .then((topic) => {
+
+      const authorized = new Authorizer(req.user, post).destroy();
+
+      if(authorized) {
+        post.destroy()
+        .then((res) => {
+          callback(null, post);
+        });
+      } else {
+        req.flash("notice", "You are not authorized to do that.")
+        callback(401);
+      }
+    })
+    .catch((err) => {
+      callback(err);
+    });
+  },
+  updatePost(req, updatedPost, callback){
     return Post.findById(id)
     .then((post) => {
       if(!post){
         return callback("Post not found");
       }
+      const authorized = new Authorizer(req.user, post).update();
 
-      post.update(updatedPost, {
-        fields: Object.keys(updatedPost)
-      })
-      .then(() => {
-        callback(null, post);
-      })
-      .catch((err) => {
-        callback(err);
-      });
+      if(authorized){
+        post.update(updatedPost, {
+          fields: Object.keys(updatedPost)
+        })
+        .then(() => {
+          callback(null, post);
+        })
+        .catch((err) => {
+          callback(err);
+        })
+      } else {
+        req.flash("notice", "You are not authorized to do that.");
+        callback("Forbidden");
+      }
     });
   }
 }
