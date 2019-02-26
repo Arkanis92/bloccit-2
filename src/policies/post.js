76826f749dd733841b408ecf0ab415292ac01024
Tanya Swift <tanyanicolerussi@gmail.com>
2019-02-26 12:14:19 -0700

    Assignment Authorization Troubleshoot 1

--- src/policies/post.js
+++ src/policies/post.js
@@ -2,23 +2,7 @@ const ApplicationPolicy = require("./application");
 
 module.exports = class PostPolicy extends ApplicationPolicy {
 
-/*  new() {
+ new() {
     return this.new();
-  }*/
-
-  create() {
-    return this.new();
-  }
-
-  edit() {
-    return this._isAdmin();
-  }
-
-  update() {
-    return this.edit();
-  }
-
-  destroy() {
-    return this.update();
   }
 }
