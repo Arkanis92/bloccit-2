8aedcf5597bcd263f8a1beb45b9e7080c0287404
Tanya Swift <tanyanicolerussi@gmail.com>
2019-03-06 13:39:05 -0700

    Checkpoint Favoriting Draft 7

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -51,14 +51,6 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "postId",
       as: "votes"
     });
-
-    Post.afterCreate((post, callback) => {
-      return models.Vote.create({
-        value: 1,
-        userId: post.userId,
-        postId: post.id
-      });
-    });
   };
 
   Post.prototype.getPoints = function(){
